ba72805c259b28adeb3e196774108884ab368ccb
Maciej <maciej.polowy1@gmail.com>
2026-01-06 19:48:23 +0100

    initial commit

---
+++ src/auth.ts
@@ -0,0 +1,6 @@
+// export const authConfig = {
+//   trustsHost: true,
+//   secret: process.env.AUTH_SECRET,
+//   session: { strategy: "jwt", maxAge: 60 * 60 * 24 * 7 },
+//   adapter: PrismaAdapter(),
+// };
